61e3d1c014ed9563e8e189804736e6d8c639feed
Ramona Häuselmann <ramona.haeuselmann@gmail.com>
2021-03-26 11:08:43 +0100

    move light with keyboard

--- lab2_rendering/skeleton.cpp
+++ lab2_rendering/skeleton.cpp
@@ -11,8 +11,8 @@ using glm::mat3;
 // ----------------------------------------------------------------------------
 // GLOBAL VARIABLES
 
-const int SCREEN_WIDTH = 500;
-const int SCREEN_HEIGHT = 500;
+const int SCREEN_WIDTH = 100;
+const int SCREEN_HEIGHT = 100;
 SDL_Surface* screen;
 int t;
 
@@ -80,13 +80,11 @@ void Update()
 	{
 		// Move camera forward
 		cameraPos.z += 0.1;
-		std::cout << "camera forward" << std::endl;
 	}
 	if( keystate[SDLK_DOWN] )
 	{
 	// Move camera backward
 		cameraPos.z -= 0.1;
-		std::cout << "camera backward" << std::endl;
 	}
 	if( keystate[SDLK_LEFT] )
 	{
@@ -100,6 +98,25 @@ void Update()
 	}
 
 	R = mat3(glm::cos(yaw), 0, glm::sin(yaw), 0, 1, 0, -glm::sin(yaw), 0, glm::cos(yaw));
+
+	if (keystate[SDLK_w]) {
+		lightPos.z += 0.1;
+	}
+	if (keystate[SDLK_s]) {
+		lightPos.z -= 0.1;
+	}
+	if (keystate[SDLK_d]) {
+		lightPos.x += 0.1;
+	}
+	if (keystate[SDLK_a]) {
+		lightPos.x -= 0.1;
+	}
+	if (keystate[SDLK_e]) {
+		lightPos.y += 0.1;
+	}
+	if (keystate[SDLK_q]) {
+		lightPos.y -= 0.1;
+	}
 }
 
 void Draw()
