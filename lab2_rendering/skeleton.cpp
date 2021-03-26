cb586ade9e60a178509481ff76adc516a6a20c74
Ramona Häuselmann <ramona.haeuselmann@gmail.com>
2021-03-26 10:25:14 +0100

    translate camera

--- lab2_rendering/skeleton.cpp
+++ lab2_rendering/skeleton.cpp
@@ -11,14 +11,17 @@ using glm::mat3;
 // ----------------------------------------------------------------------------
 // GLOBAL VARIABLES
 
-const int SCREEN_WIDTH = 500;
-const int SCREEN_HEIGHT = 500;
+const int SCREEN_WIDTH = 100;
+const int SCREEN_HEIGHT = 100;
 SDL_Surface* screen;
 int t;
 
 std::vector<Triangle> triangles;
-float focalLength = 500.0f;
-glm::vec3 cameraPos(0, 0, -3.0f);
+// float focalLength = 500.0f;
+// glm::vec3 cameraPos(0, 0, -3.0f);
+
+float focalLength = 2.0f * SCREEN_HEIGHT / 2.0f;
+glm::vec3 cameraPos(0, 0, -(2.0f * focalLength / SCREEN_HEIGHT) - 1.0f);
 
 // ----------------------------------------------------------------------------
 // STRUCTS
@@ -64,6 +67,30 @@ void Update()
 	float dt = float(t2-t);
 	t = t2;
 	cout << "Render time: " << dt << " ms." << endl;
+
+	Uint8* keystate = SDL_GetKeyState( 0 );
+	if( keystate[SDLK_UP] )
+	{
+		// Move camera forward
+		cameraPos.z += 0.1;
+		std::cout << "camera forward" << std::endl;
+	}
+	if( keystate[SDLK_DOWN] )
+	{
+	// Move camera backward
+		cameraPos.z -= 0.1;
+		std::cout << "camera backward" << std::endl;
+	}
+	if( keystate[SDLK_LEFT] )
+	{
+	// Move camera to the left
+		cameraPos.x -= 0.1;
+	}
+	if( keystate[SDLK_RIGHT] )
+	{
+	// Move camera to the right
+		cameraPos.x += 0.1;
+	}
 }
 
 void Draw()
